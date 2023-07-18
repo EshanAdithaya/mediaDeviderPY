cb97b18d6ad552b90923951e2523dffa7238df81
Eshan Adithaya <87906517+EshanAdithaya@users.noreply.github.com>
2023-07-18 17:12:13 +0530

    fresh first upload (source code)

---
+++ Year_Date_Wise_Save.py
@@ -0,0 +1,30 @@
+import os
+import shutil
+from datetime import datetime
+
+# Prompt the user for the location of the media files
+location = input("Enter the location of the media files: ")
+
+# Create a new directory to store the organized media files
+organized_folder = "Organized Media Files"
+os.makedirs(organized_folder, exist_ok=True)
+
+# Loop through all the files in the original location
+for filename in os.listdir(location):
+    # Get the full path of the file
+    filepath = os.path.join(location, filename)
+    
+    # Get the modification timestamp of the file
+    timestamp = os.path.getmtime(filepath)
+    modified_date = datetime.fromtimestamp(timestamp)
+    
+    # Create the year and month folders if they don't exist
+    year_folder = os.path.join(organized_folder, str(modified_date.year))
+    os.makedirs(year_folder, exist_ok=True)
+    month_folder = os.path.join(year_folder, modified_date.strftime("%B"))
+    os.makedirs(month_folder, exist_ok=True)
+    
+    # Copy the file to the appropriate folder
+    shutil.copy2(filepath, month_folder)
+    
+print("Media files organized successfully!")
